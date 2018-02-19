37fe41c0490a5526b5245a7b4d461baecf5701cd
ernesto <ernesto@signature-it.com>
2018-02-19 17:39:52 +0200

    convert from laravel 5.5 to 5.6 (part 2)

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -17,7 +17,7 @@ class ExampleTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/eloquent/collection')
-                    ->assertSee('Eloquent Collection');
+                    ->assertSee('Laravel');
         });
     }
 }
